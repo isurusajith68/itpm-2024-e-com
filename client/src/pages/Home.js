e17ea82e436c1625eb4b5ecf61d4c568264b960c
IT21158254 <IT21158254@my.sliit.lk>
2024-08-22 23:29:47 +0530

    promotion and single product disply done

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,43 @@
+import { useEffect, useState } from "react";
 import HeadLineCards from "../components/HeadLineCards";
 import Hero from "../components/Hero";
 import NavBar from "../components/HomeNavBar";
 import Promotions from "../components/Promotions";
 
 const Home = () => {
+  const [product, setProduct] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const res = await fetch("http://localhost:5000/products");
+        const data = await res.json();
+        setProduct(data.products);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchProduct();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
+      </div>
+    );
+  }
   return (
     <div>
       <NavBar />
       <Hero />
-      <HeadLineCards />
+      <HeadLineCards 
+      product={product}
+      setLoading={setLoading} />
       <Promotions />
     </div>
   );
