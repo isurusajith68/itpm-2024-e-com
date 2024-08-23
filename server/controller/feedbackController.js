f2ab5ece6b3a7b5ad4066cbb350d01b3b0bf463a
IT21158254 <IT21158254@my.sliit.lk>
2024-08-23 23:03:16 +0530

    asd

--- server/controller/feedbackController.js
+++ server/controller/feedbackController.js
@@ -29,6 +29,16 @@ export const createFeedback = async (req, res) => {
   }
 };
 
+export const getFeedBackssByUserId = async (req, res) => { 
+  try {
+    const feedbacks = await Feedback.find({ user: req.params.id });
+
+    res.status(200).json(feedbacks);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
+
 export const deleteFeedback = async (req, res) => {
   try {
     const { id } = req.params;
