f4ee1d0493e884c5010446c4cdf76404f31999db
Isuru Sajith <89340506+isurusajith68@users.noreply.github.com>
2024-08-24 14:18:39 +0530

    refactor: Update navigation links in Layout, Login, and SideBar components
    The code changes in Layout.js, Login.js, and SideBar.js update the navigation links to ensure they point to the correct routes. In Layout.js, the useEffect hook is used to redirect users based on their role. In Login.js, the navigate function is used to redirect users based on their role after successful login. In SideBar.js, the navigate function is used to redirect users based on their role. These updates improve the user experience by ensuring that the navigation links lead to the intended destinations.
    
    This commit message follows the established convention of using a prefix (refactor) to indicate code refactoring or improvements.

--- client/src/components/SideBar.js
+++ client/src/components/SideBar.js
@@ -46,128 +46,141 @@ const SideBar = () => {
         </Link>
         <Divider className="mt-4" />
         <div className="flex flex-col">
-          <div className="flex flex-col px-4 mt-2 gap-4 border-t p-2 border-white">
-            <Link
-              to="/dashboard/staff"
-              className={
-                pathname.startsWith("/dashboard/staff")
-                  ? "p-2  flex gap-2 text-blue-600 hover:bg-blue-800 hover:rounded-lg border transition duration-300 border-white shadow-sm bg-blue-900 rounded-lg cursor-pointer  items-center"
-                  : "p-2  flex gap-2 hover:bg-blue-500 hover:rounded-lg border transition duration-300  border-white shadow-sm bg-blue-700 rounded-lg cursor-pointer  items-center"
-              }
-            >
-              <IoMdPersonAdd size={20} className="text-white" />
-              <span className="ml-2 text-white">Staff Manager</span>
-            </Link>
-          </div>
-          <div className="flex flex-col px-4  gap-4 border-t p-2 border-white">
-            <Link
-              to="/dashboard/salary"
-              className={
-                pathname.startsWith("/dashboard/salary")
-                  ? "p-2  flex gap-2 text-blue-600 hover:bg-blue-800 hover:rounded-lg border transition duration-300 border-white shadow-sm bg-blue-900 rounded-lg cursor-pointer  items-center"
-                  : "p-2  flex gap-2 hover:bg-blue-500 hover:rounded-lg border transition duration-300  border-white shadow-sm bg-blue-700 rounded-lg cursor-pointer  items-center"
-              }
-            >
-              <MdOutlinePriceChange size={20} className="text-white" />
-              <span className="ml-2 text-white">Salary Management</span>
-            </Link>
-          </div>
-          <div className="flex flex-col px-4 gap-4 border-t p-2 border-white ">
-            <Link
-              to="/dashboard/products"
-              className={
-                pathname.startsWith("/dashboard/products")
-                  ? "p-2  flex gap-2 text-blue-800 hover:bg-blue-500 hover:rounded-lg border transition duration-300 border-white shadow-sm bg-blue-900 rounded-lg cursor-pointer items-center"
-                  : "p-2  flex gap-2 hover:bg-blue-500 hover:rounded-lg border transition duration-300 border-white shadow-sm bg-blue-700 rounded-lg cursor-pointer  items-center"
-              }
-            >
-              <MdPriceChange size={20} className="text-white" />
-              <span className="ml-2 text-white">Products Manager</span>
-            </Link>
-          </div>
-
-          <div className="flex flex-col px-4 gap-4 border-t p-2 border-white ">
-            <Link
-              to="/dashboard/supply"
-              className={
-                pathname.startsWith("/dashboard/supply")
-                  ? "p-2  flex gap-2 text-blue-800 hover:bg-blue-500 hover:rounded-lg border transition duration-300 border-white shadow-sm bg-blue-900 rounded-lg cursor-pointer items-center"
-                  : "p-2  flex gap-2 hover:bg-blue-500 hover:rounded-lg border transition duration-300 border-white shadow-sm bg-blue-700 rounded-lg cursor-pointer  items-center"
-              }
-            >
-              <GiProgression size={20} className="text-white" />
-              <span className="ml-2 text-white">Supply Manager</span>
-            </Link>
-          </div>
-
-          <div className="flex flex-col px-4 gap-4 border-t p-2 border-white ">
-            <Link
-              to="/dashboard/promotion"
-              className={
-                pathname.startsWith("/dashboard/promotion")
-                  ? "p-2  flex gap-2 text-blue-800 hover:bg-blue-500 hover:rounded-lg border transition duration-300 border-white shadow-sm bg-blue-900 rounded-lg cursor-pointer items-center"
-                  : "p-2  flex gap-2 hover:bg-blue-500 hover:rounded-lg border transition duration-300 border-white shadow-sm bg-blue-700 rounded-lg cursor-pointer  items-center"
-              }
-            >
-              <CiDiscount1 size={20} className="text-white" />
-              <span className="ml-2 text-white">Promotion Manager</span>
-            </Link>
-          </div>
-
-          <div className="flex flex-col px-4 gap-4 border-t p-2 border-white ">
-            <Link
-              to="/dashboard/sales"
-              className={
-                pathname.startsWith("/dashboard/sales")
-                  ? "p-2  flex gap-2 text-blue-800 hover:bg-blue-500 hover:rounded-lg border transition duration-300 border-white shadow-sm bg-blue-900 rounded-lg cursor-pointer items-center"
-                  : "p-2  flex gap-2 hover:bg-blue-500 hover:rounded-lg border transition duration-300 border-white shadow-sm bg-blue-700 rounded-lg cursor-pointer  items-center"
-              }
-            >
-              <FcSalesPerformance size={20} className="text-white" />
-              <span className="ml-2 text-white">Sales Manager</span>
-            </Link>
-          </div>
-          <div className="flex flex-col px-4 gap-4 border-t p-2 border-white ">
-            <Link
-              to="/dashboard/delivery"
-              className={
-                pathname.startsWith("/dashboard/order")
-                  ? "p-2  flex gap-2 text-blue-800 hover:bg-blue-500 hover:rounded-lg border transition duration-300 border-white shadow-sm bg-blue-900 rounded-lg cursor-pointer items-center"
-                  : "p-2  flex gap-2 hover:bg-blue-500 hover:rounded-lg border transition duration-300 border-white shadow-sm bg-blue-700 rounded-lg cursor-pointer  items-center"
-              }
-            >
-              <CiDeliveryTruck size={20} className="text-white" />
-              <span className="ml-2 text-white">Order Manager</span>
-            </Link>
-          </div>
-
-          <div className="flex flex-col px-4 gap-4 border-t p-2 border-white ">
-            <Link
-              to="/dashboard/feedback"
-              className={
-                pathname.startsWith("/dashboard/feedback")
-                  ? "p-2  flex gap-2 text-blue-800 hover:bg-blue-500 hover:rounded-lg border transition duration-300 border-white shadow-sm bg-blue-900 rounded-lg cursor-pointer items-center"
-                  : "p-2  flex gap-2 hover:bg-blue-500 hover:rounded-lg border transition duration-300 border-white shadow-sm bg-blue-700 rounded-lg cursor-pointer  items-center"
-              }
-            >
-              <FcFeedback size={20} className="text-white" />
-              <span className="ml-2 text-white">Feedback Manager</span>
-            </Link>
-          </div>
-
-          <div className="flex flex-col px-4 gap-4 border-t p-2 border-white ">
-            <Link
-              to="/dashboard/service"
-              className={
-                pathname.startsWith("/dashboard/service")
-                  ? "p-2  flex gap-2 text-blue-800 hover:bg-blue-500 hover:rounded-lg border transition duration-300 border-white shadow-sm bg-blue-900 rounded-lg cursor-pointer items-center"
-                  : "p-2  flex gap-2 hover:bg-blue-500 hover:rounded-lg border transition duration-300 border-white shadow-sm bg-blue-700 rounded-lg cursor-pointer  items-center"
-              }
-            >
-              <MdMiscellaneousServices size={20} className="text-white" />
-              <span className="ml-2 text-white">Service Manager</span>
-            </Link>
-          </div>
+          {user?.role === "admin" && (
+            <div className="flex flex-col px-4 mt-2 gap-4 border-t p-2 border-white">
+              <Link
+                to="/dashboard/staff"
+                className={
+                  pathname.startsWith("/dashboard/staff")
+                    ? "p-2  flex gap-2 text-blue-600 hover:bg-blue-800 hover:rounded-lg border transition duration-300 border-white shadow-sm bg-blue-900 rounded-lg cursor-pointer  items-center"
+                    : "p-2  flex gap-2 hover:bg-blue-500 hover:rounded-lg border transition duration-300  border-white shadow-sm bg-blue-700 rounded-lg cursor-pointer  items-center"
+                }
+              >
+                <IoMdPersonAdd size={20} className="text-white" />
+                <span className="ml-2 text-white">Staff Manager</span>
+              </Link>
+            </div>
+          )}
+          {user?.role === "admin" && (
+            <div className="flex flex-col px-4  gap-4 border-t p-2 border-white">
+              <Link
+                to="/dashboard/salary"
+                className={
+                  pathname.startsWith("/dashboard/salary")
+                    ? "p-2  flex gap-2 text-blue-600 hover:bg-blue-800 hover:rounded-lg border transition duration-300 border-white shadow-sm bg-blue-900 rounded-lg cursor-pointer  items-center"
+                    : "p-2  flex gap-2 hover:bg-blue-500 hover:rounded-lg border transition duration-300  border-white shadow-sm bg-blue-700 rounded-lg cursor-pointer  items-center"
+                }
+              >
+                <MdOutlinePriceChange size={20} className="text-white" />
+                <span className="ml-2 text-white">Salary Management</span>
+              </Link>
+            </div>
+          )}
+          {(user?.role === "inventory" || user?.role === "admin") && (
+            <div className="flex flex-col px-4 gap-4 border-t p-2 border-white ">
+              <Link
+                to="/dashboard/products"
+                className={
+                  pathname.startsWith("/dashboard/products")
+                    ? "p-2  flex gap-2 text-blue-800 hover:bg-blue-500 hover:rounded-lg border transition duration-300 border-white shadow-sm bg-blue-900 rounded-lg cursor-pointer items-center"
+                    : "p-2  flex gap-2 hover:bg-blue-500 hover:rounded-lg border transition duration-300 border-white shadow-sm bg-blue-700 rounded-lg cursor-pointer  items-center"
+                }
+              >
+                <MdPriceChange size={20} className="text-white" />
+                <span className="ml-2 text-white">Products Manager</span>
+              </Link>
+            </div>
+          )}
+          {(user?.role === "suppliers" || user?.role === "admin") && (
+            <div className="flex flex-col px-4 gap-4 border-t p-2 border-white ">
+              <Link
+                to="/dashboard/supply"
+                className={
+                  pathname.startsWith("/dashboard/supply")
+                    ? "p-2  flex gap-2 text-blue-800 hover:bg-blue-500 hover:rounded-lg border transition duration-300 border-white shadow-sm bg-blue-900 rounded-lg cursor-pointer items-center"
+                    : "p-2  flex gap-2 hover:bg-blue-500 hover:rounded-lg border transition duration-300 border-white shadow-sm bg-blue-700 rounded-lg cursor-pointer  items-center"
+                }
+              >
+                <GiProgression size={20} className="text-white" />
+                <span className="ml-2 text-white">Supply Manager</span>
+              </Link>
+            </div>
+          )}
+          {(user?.role === "promotion" || user?.role === "admin") && (
+            <div className="flex flex-col px-4 gap-4 border-t p-2 border-white ">
+              <Link
+                to="/dashboard/promotion"
+                className={
+                  pathname.startsWith("/dashboard/promotion")
+                    ? "p-2  flex gap-2 text-blue-800 hover:bg-blue-500 hover:rounded-lg border transition duration-300 border-white shadow-sm bg-blue-900 rounded-lg cursor-pointer items-center"
+                    : "p-2  flex gap-2 hover:bg-blue-500 hover:rounded-lg border transition duration-300 border-white shadow-sm bg-blue-700 rounded-lg cursor-pointer  items-center"
+                }
+              >
+                <CiDiscount1 size={20} className="text-white" />
+                <span className="ml-2 text-white">Promotion Manager</span>
+              </Link>
+            </div>
+          )}
+          {(user?.role === "sales" || user?.role === "admin") && (
+            <div className="flex flex-col px-4 gap-4 border-t p-2 border-white ">
+              <Link
+                to="/dashboard/sales"
+                className={
+                  pathname.startsWith("/dashboard/sales")
+                    ? "p-2  flex gap-2 text-blue-800 hover:bg-blue-500 hover:rounded-lg border transition duration-300 border-white shadow-sm bg-blue-900 rounded-lg cursor-pointer items-center"
+                    : "p-2  flex gap-2 hover:bg-blue-500 hover:rounded-lg border transition duration-300 border-white shadow-sm bg-blue-700 rounded-lg cursor-pointer  items-center"
+                }
+              >
+                <FcSalesPerformance size={20} className="text-white" />
+                <span className="ml-2 text-white">Sales Manager</span>
+              </Link>
+            </div>
+          )}
+          {(user?.role === "delivery" || user?.role === "admin") && (
+            <div className="flex flex-col px-4 gap-4 border-t p-2 border-white ">
+              <Link
+                to="/dashboard/delivery"
+                className={
+                  pathname.startsWith("/dashboard/order")
+                    ? "p-2  flex gap-2 text-blue-800 hover:bg-blue-500 hover:rounded-lg border transition duration-300 border-white shadow-sm bg-blue-900 rounded-lg cursor-pointer items-center"
+                    : "p-2  flex gap-2 hover:bg-blue-500 hover:rounded-lg border transition duration-300 border-white shadow-sm bg-blue-700 rounded-lg cursor-pointer  items-center"
+                }
+              >
+                <CiDeliveryTruck size={20} className="text-white" />
+                <span className="ml-2 text-white">Order Manager</span>
+              </Link>
+            </div>
+          )}
+          {(user?.role === "feedback" || user?.role === "admin") && (
+            <div className="flex flex-col px-4 gap-4 border-t p-2 border-white ">
+              <Link
+                to="/dashboard/feedback"
+                className={
+                  pathname.startsWith("/dashboard/feedback")
+                    ? "p-2  flex gap-2 text-blue-800 hover:bg-blue-500 hover:rounded-lg border transition duration-300 border-white shadow-sm bg-blue-900 rounded-lg cursor-pointer items-center"
+                    : "p-2  flex gap-2 hover:bg-blue-500 hover:rounded-lg border transition duration-300 border-white shadow-sm bg-blue-700 rounded-lg cursor-pointer  items-center"
+                }
+              >
+                <FcFeedback size={20} className="text-white" />
+                <span className="ml-2 text-white">Feedback Manager</span>
+              </Link>
+            </div>
+          )}
+          {(user?.role === "service" || user?.role === "admin") && (
+            <div className="flex flex-col px-4 gap-4 border-t p-2 border-white ">
+              <Link
+                to="/dashboard/service"
+                className={
+                  pathname.startsWith("/dashboard/service")
+                    ? "p-2  flex gap-2 text-blue-800 hover:bg-blue-500 hover:rounded-lg border transition duration-300 border-white shadow-sm bg-blue-900 rounded-lg cursor-pointer items-center"
+                    : "p-2  flex gap-2 hover:bg-blue-500 hover:rounded-lg border transition duration-300 border-white shadow-sm bg-blue-700 rounded-lg cursor-pointer  items-center"
+                }
+              >
+                <MdMiscellaneousServices size={20} className="text-white" />
+                <span className="ml-2 text-white">Service Manager</span>
+              </Link>
+            </div>
+          )}
         </div>
       </div>
 
--- client/src/layout/Layout.js
+++ client/src/layout/Layout.js
@@ -1,13 +1,25 @@
+import { useEffect } from "react";
 import DashboardNavbar from "../components/DashboardNavbar";
 import SideBar from "../components/SideBar";
 
 const Layout = ({ children }) => {
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("authUser"));
+    if (!user) {
+      window.location.href = "/login";
+    } else if (user.role === "user") {
+      window.location.href = "/";
+    }
+  }, []);
+
   return (
     <div className="flex w-screen min-h-screen bg-[#eef3fd] p-4 ">
       <SideBar />
       <div className="flex flex-col w-full pl-5  gap-4">
         <DashboardNavbar />
-        <div className="flex-grow bg-white  rounded-lg shadow-lg">{children}</div>
+        <div className="flex-grow bg-white  rounded-lg shadow-lg">
+          {children}
+        </div>
       </div>
     </div>
   );
