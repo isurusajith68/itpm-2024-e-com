8ca7f4fc45160b129574fcbf6ef8103fef779619
IT21158254 <IT21158254@my.sliit.lk>
2024-10-03 13:14:50 +0530

    update feedback done

--- server/router/feedbackRouter.js
+++ server/router/feedbackRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { createFeedback, deleteFeedback, getFeedback } from "../controller/feedbackController.js";
+import { createFeedback, deleteFeedback, getFeedback, updateFeedBack } from "../controller/feedbackController.js";
 
 const feedbackRouter = express.Router();
 
 feedbackRouter.get("/", getFeedback);
 feedbackRouter.post("/", createFeedback);
 feedbackRouter.delete("/:id", deleteFeedback);
+feedbackRouter.put("/:id", updateFeedBack);
 
 
 export default feedbackRouter;
